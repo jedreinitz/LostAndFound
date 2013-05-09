1f53930c40ba429593415ca33a58ff7631f60c50
atmiguel <atmiguel@mit.edu>
2013-05-09 00:18:00 -0400

    users and passwords working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user
+  before_filter :add_value_to_session
+
+  private
+  def add_value_to_session
+    session[:message] = "Hello World!"
+  end
+
+  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
