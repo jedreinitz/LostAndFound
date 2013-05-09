6bc3d56b304395a582fe47ff9bf37082bf3c3190
atmiguel <atmiguel@mit.edu>
2013-05-08 23:51:34 -0400

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LostAndFound::Application.config.secret_token = 'e8972a5ae43395b986f21a9affa5d6a5176837a7ba846979cf64d3fefde12ed668683640ab609530bd945c7ad5573640e8c3b0385bdd06676a9d20c4e0f56b41'
